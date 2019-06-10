3df2d819bbe84fe26d7cb651e8d9fb17a94b32cb
Dave Draper <dave.draper20@googlemail.com>
2019-06-10 15:15:03 +1000

    Minor FieldsReducer refactor

--- src/reducers/FieldsReducer.js
+++ src/reducers/FieldsReducer.js
@@ -2,6 +2,9 @@
 import {
   type FormState,
   type FieldsReducerActions,
+  type FieldsReducerRegisterFieldAction,
+  type FieldsReducerUpdateFieldValueAction,
+  type FieldsReducerSetOptionsAction,
   SET_FIELD_OPTIONS_ACTION,
   REGISTER_FIELD_ACTION,
   UPDATE_FIELD_VALUE_ACTION
@@ -12,43 +15,64 @@ function initFieldsReducer({ fields }: FormState) {
   return { fields };
 }
 
-// TODO: Need
+const registerField = (
+  state: FormState,
+  action: FieldsReducerRegisterFieldAction
+) => {
+  const { field } = action.payload;
+  const { fields } = state;
+  const fieldIsRegistered = fields.find(
+    existingField => field.id === existingField.id
+  );
+  if (fieldIsRegistered) {
+    return state;
+  }
+  // TODO: needed to spread the field when registering - needs deep clone really
+  //       to avoid issues with attributes that are complex objects
+  return { ...state, fields: [...fields, { ...field }] };
+};
+
+const setFieldOptions = (
+  state: FormState,
+  action: FieldsReducerSetOptionsAction
+) => {
+  return state;
+};
+
+const updateFieldValue = (
+  state: FormState,
+  action: FieldsReducerUpdateFieldValueAction
+) => {
+  const { id, value } = action.payload;
+  const { fields } = state;
+  const field = fields.find(field => field.id === id);
+  if (field) {
+    field.value = value;
+    // TODO: Create new fields array?
+    // NOTE: Need to return a new object to trigger re-render
+
+    // TODO: Should also update the overall form state,
+    //       - overall value
+    //       - validity
+    //       - etc
+    return { ...state };
+  }
+  return state;
+};
+
 function fieldsReducer(state: FormState, action: FieldsReducerActions) {
   switch (action.type) {
-    case SET_FIELD_OPTIONS_ACTION:
-      return state;
+    case SET_FIELD_OPTIONS_ACTION: {
+      return setFieldOptions(state, action);
+    }
     case REGISTER_FIELD_ACTION: {
-      const { field } = action.payload;
-      const { fields } = state;
-      const fieldIsRegistered = fields.find(
-        existingField => field.id === existingField.id
-      );
-      if (fieldIsRegistered) {
-        return state;
-      }
-      // TODO: needed to spread the field when registering - needs deep clone really
-      //       to avoid issues with attributes that are complex objects
-      return { ...state, fields: [...fields, { ...field }] };
+      return registerField(state, action);
     }
     case UPDATE_FIELD_VALUE_ACTION: {
-      const { id, value } = action.payload;
-      const { fields } = state;
-      const field = fields.find(field => field.id === id);
-      if (field) {
-        field.value = value;
-        // TODO: Create new fields array?
-        // NOTE: Need to return a new object to trigger re-render
-
-        // TODO: Should also update the overall form state,
-        //       - overall value
-        //       - validity
-        //       - etc
-        return { ...state };
-      }
-      return state;
+      return updateFieldValue(state, action);
     }
     default:
-      return { ...state };
+      return state;
   }
 }
 
